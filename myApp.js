cf93b071606e4cef927227c07269b8c9477f5083
Raycmv <raymondcmv@gmail.com>
2024-04-03 13:36:31 +0200

    agregando un middleware

--- myApp.js
+++ myApp.js
@@ -7,8 +7,15 @@ let jsonObj = {"message": "Hello json"}
 absPathhtml = __dirname + '/views/index.html'
 absPathCss = __dirname + '/public'
 
+//middlewares
 app.use('/public',express.static(absPathCss))
+app.use((req, res, next)=>{
+    console.log(req.method, req.path,'-', req.ip);
+    next();
+})
 
+
+//routes
 app.get('/',(req, res)=>{
     res.sendFile(absPathhtml)
 })
@@ -16,9 +23,11 @@ app.get('/',(req, res)=>{
 app.get('/json',(req, res)=>{
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         res.json(jsonObj.message.toUpperCase())
-      } else {
+    } else if(process.env.MESSAGE_STYLE === 'lowercase'){
+        res.json(jsonObj.message.toLowerCase())
+    } else {
         res.json(jsonObj)
-      }
+    }
     
 })
 
