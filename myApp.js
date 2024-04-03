cd0493163cffa76191f7cc19709190c6aabe9fad
Raycmv <raymondcmv@gmail.com>
2024-04-03 11:26:32 +0200

    agregando el css

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 let express = require('express');
 let app = express();
 
+absPath = __dirname + '/views/index.html'
 
+absPathCss = __dirname + '/public'
+
+app.use('/public',express.static(absPathCss))
+
+app.get('/',(req, res)=>{
+    res.sendFile(absPath)
+})
 
 
 
