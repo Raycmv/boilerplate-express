42b6eeaf5edc793e6de3bf92f094abc9a770bd5b
Raycmv <164234436+Raycmv@users.noreply.github.com>
2024-04-16 09:53:45 +0200

    Update myApp.js
    finishing

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
@@ -8,15 +8,12 @@ let jsonObj = {"message": "Hello json"}
 absPathhtml = __dirname + '/views/index.html'
 absPathCss = __dirname + '/public'
 
-//root-level middlewares
 app.use('/public',express.static(absPathCss))
 app.use((req, res, next)=>{
     console.log(req.method, req.path,'-', req.ip);
     next();
 })
 
-
-//routes
 app.get('/',(req, res)=>{
     res.sendFile(absPathhtml)
 })
@@ -24,36 +21,31 @@ app.get('/',(req, res)=>{
 app.get('/json',(req, res)=>{
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         res.json(jsonObj.message.toUpperCase())
-    } 
-    else if(process.env.MESSAGE_STYLE === 'lowercase'){
+    } else if(process.env.MESSAGE_STYLE === 'lowercase'){
         res.json(jsonObj.message.toLowerCase())
-    } 
-    else { res.json(jsonObj) }
+    } else {
+        res.json(jsonObj)
+    }
 })
 
-//using route parameters.
-app.get("/:param/echo", (req, res) => {
-    const {param} = req.params;
-    res.json({ echo: param });
+app.get("/:word/echo", (req, res) => {
+    const {word} = req.params;
+    res.json({ echo: word });
 });
 
-//middleware mounted at a specific route
 app.get('/now',(req, res, next)=>{
-    req.time = new Date().toString();
+    req.time = new Date().toString()
     next();
   },(req, res)=>res.send(req.time)
 )
 
-// middleware to parse the request body
-app.use(bodyParser.urlencoded({extended: false}))   
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.post("/name", (req, res) => {
-    res.json(req.body)		//json format
     const { first, last } = req.body;
-    res.send('Name: ' + first + ' Last name: ' + last)		//string format
+    let name = first +" "+ last;
+    res.json({"name":name})
 });
-
-
 
 
  module.exports = app;
